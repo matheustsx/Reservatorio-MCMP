5120f7e608c96ebd3416bff42689689f267a7422
matheustsx <matheusbatistachavante@gmail.com>
2024-04-09 09:20:05 -0300

    feat: criando e organizando o inicio do novo ambiente do projeto

---
+++ Espressif32-MCMP/include/main.h
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#define EXAMPLE_ESP_WIFI_SSID      "mywifissid"
+#define EXAMPLE_ESP_WIFI_PASS      "mywifipass"
+#define EXAMPLE_ESP_WIFI_CHANNEL   1
+#define EXAMPLE_MAX_STA_CONN       4
